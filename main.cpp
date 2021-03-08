c8eb3129ce60bb6c21c261ed0a2da2ddae69d1c2
zhangjun01 <zhangjun01@cobotsys.com>
2021-03-08 13:58:46 +0800

    .

--- main.cpp
+++ main.cpp
@@ -1,11 +1,6 @@
 #include <iostream>
-#include "rttr/detail/base/version.h"
-#include "json_serialization/from_json.h"
-#include "mysql_connect.h"
 
 int main() {
-    std::cout<<RTTR_VERSION;
     std::cout << "Hello, World!" << std::endl;
-    execute();
     return 0;
 }
